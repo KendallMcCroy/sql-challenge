fc55ec6736100ecd9cee8d13b21353be3eed22b3
Kendall <kdmccroy@gmail.com>
2021-12-13 17:45:54 -0500

    query question 1

--- EmployeeSQL/QuickDBD-export.sql
+++ EmployeeSQL/QuickDBD-export.sql
@@ -78,3 +78,13 @@ REFERENCES "titles" ("title_id");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+	
+
+--------------------------------------------------------------------------------------------
+---------------------------------- Queries--------------------------------------------------
+--------------------------------------------------------------------------------------------
+	
+--1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+select employees.emp_no as  employee_number, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+join salaries on employees.emp_no = salaries.emp_no
