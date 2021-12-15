6f8b8e354dbffe50e6ecdd032eb463f2ef746112
Kendall <kdmccroy@gmail.com>
2021-12-15 01:51:04 -0500

    Added Query 7 and 8

--- EmployeeSQL/QuickDBD-export.sql
+++ EmployeeSQL/QuickDBD-export.sql
@@ -118,15 +118,31 @@ REFERENCES "employees" ("emp_no");
 -- WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
 	
 -- 6. List all employees in the Sales department, includingg their employee number, last name, first name, and department name
-SELECT departments.dept_name, employees.last_name, employees.first_name, employees.emp_no
-FROM dept_emp
-JOIN departments
-ON dept_emp.dept_no = departments.dept_no
-JOIN employees	
-ON dept_emp.emp_no = employees.emp_no	
-WHERE departments.dept_name = 'Sales'
+-- SELECT departments.dept_name, employees.last_name, employees.first_name, employees.emp_no
+-- FROM dept_emp
+-- JOIN departments
+-- ON dept_emp.dept_no = departments.dept_no
+-- JOIN employees	
+-- ON dept_emp.emp_no = employees.emp_no	
+-- WHERE departments.dept_name = 'Sales'
+
+	
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
+-- SELECT departments.dept_name, employees.last_name, employees.first_name, employees.emp_no
+-- FROM dept_emp
+-- JOIN departments
+-- ON dept_emp.dept_no = departments.dept_no
+-- JOIN employees	
+-- ON dept_emp.emp_no = employees.emp_no	
+-- WHERE departments.dept_name = 'Sales' or departments.dept_name = 'Development'
 	
 	
+	
+-- 8. In descrending order, list the frequency count of employee last names, i.e., how many employees share each last name
+-- SELECT employees.last_name, Count (*)
+-- FROM employees
+-- GROUP BY employees. last_name
+-- ORDER BY COUNT (*) DESC;
 	
 	
 	
